6308f7c7a1e2ebdb801e39244f5deb61428ca59c
anekkumarsingh <anekkumarsingh01@gmail.com>
2026-09-24 11:25:20 +0530

    problem with sequential to_thread , rise of gather

--- python-core/async-io/file_handling/4.read_mulitple_files_asyncio.py
+++ python-core/async-io/file_handling/4.read_mulitple_files_asyncio.py
@@ -1,16 +1,22 @@
 import asyncio
 import os,threading
 
+
+"""
+What is the problem with the code ?
+Here asyncio.to_thread is being awaited sequentially  ,hence in o/p ,you will see one thread running at a time .
+
+sol:  use gather
+"""
+
 def get_thread_info(label):
     print(f'{label}  ,thread name : {threading.current_thread().name} , is_alive :: {threading.current_thread().is_alive()}')
 
-def read_files_sync(file_paths :list):
+def read_files_sync(file_path):
     get_thread_info('read_files_sync STARTED')
-    file_result = []
-    for file_path in file_paths:
-     with open(file_path,'r') as f:
-          file_result.append(f.read())
-    return file_result
+    with open(file_path,'r') as f:
+        return  f.read()
+
 
 async def main():
     file_paths = [
@@ -18,7 +24,7 @@ async def main():
         '/Users/anekkumarsingh/PycharmProjects/my-app/pythonProject/.gitignore',
         '/Users/anekkumarsingh/PycharmProjects/my-app/pythonProject/README.md'
     ]
-    return await asyncio.to_thread(read_files_sync,file_paths) 
+    return [await asyncio.to_thread(read_files_sync,file_path) for file_path in file_paths]
 
 
 
